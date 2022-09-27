aa5ec1ac10cd43fe9b24762fd34eab6d2113bd22
mateusvg <mateusvitorino@gmail.com>
2022-09-27 09:08:28 -0300

    home-grid fetch api

--- src/good-luck/src/pages/Home.tsx
+++ src/good-luck/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import * as  React from 'react';
 import { useState, useEffect } from "react";
+import GridHome from '../componets/GridHome'
 
 type resultProps = {
   idRaffle: number;
@@ -12,36 +13,8 @@ type resultProps = {
 
 
 export default function Home() {
-  const [result, setResult] = useState<resultProps[]>([]);
-
-  useEffect(() => {
-    const apiGetRaffles = async () => {
-      const data = await fetch("http://localhost:8080/raffle/all", {
-        method: "GET"
-      });
-      const jsonData = await data.json();
-      setResult(jsonData);
-    };
-    apiGetRaffles();
-  }, []);
 
   return (
-    <div className="Home">
-      <h1>
-        {result?.map(value => {
-          return (
-
-              <ul>
-                {value.idRaffle}
-                {value.RaffleName}
-                {value.RaffleParticipants}
-                {value.date}
-                {value.description}
-              </ul>
-
-          );
-        })}
-      </h1>
-    </div>
+    <GridHome/>
   );
 }
