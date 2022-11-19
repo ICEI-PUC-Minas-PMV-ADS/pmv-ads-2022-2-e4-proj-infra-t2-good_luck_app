07bf2c6884fddf4f960741ae1c673fcc5f7ccc5c
mateusvg <mateusvitorino@outlook.com.br>
2022-11-19 15:32:35 -0300

    tela de cadastro

---
+++ src/good-luck/src/mobile/src/screens/Cadastro.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ImageBackground,
+  Button,
+  Text,
+  TextInput
+} from 'react-native';
+
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Tab = createBottomTabNavigator();
+  
+export default function (props) {
+    const simpleAlertHandler = () => {
+    alert('Usuario Cadastrado');
+  };
+
+  const uri2 = `https://the-news-back-end.herokuapp.com/user/register`
+  
+  const cadastrar = async () => {
+      const resp = await fetch(uri2, {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nome, email, senha }),
+      });
+      simpleAlertHandler()
+      props.navigation.navigate('Login')
+  };
+
+const [nome, setNome] = useState('')
+const [email, setEmail] = useState('')
+const [senha, setSenha] = useState('')
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={{textAlign:'center', fontSize:25, marginBottom:50}}>Vamos sortear... </Text>
+
+      <TextInput style={styles.input} placeholder="Nome" onChangeText={(text) => setNome(text)} />
+      <TextInput style={styles.input} placeholder="Email" onChangeText={(text) => setEmail(text)} />
+      <TextInput style={styles.input} placeholder="Senha" secureTextEntry={true} onChangeText={(text) => setSenha(text)} />
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => cadastrar()}>
+        <Text style={styles.text}>Cadastrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    paddingBottom: '10%',
+  },
+
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: 10,
+    paddingVertical: 12,
+    paddingHorizontal: 32,
+    borderRadius: 20,
+    elevation: 3,
+    backgroundColor: '#7B68EE',
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: 'bold',
+    letterSpacing: 0.25,
+    color: 'white',
+  },
+
+  input: {
+    backgroundColor: '#FFF',
+    marginTop:15,
+    marginBottom: 8,
+    height:50,
+    textAlign:'center',
+    fontSize:20,
+    borderBottomWidth:1,
+    borderBottomColor:'black',
+
+    
+  },
+
+});
